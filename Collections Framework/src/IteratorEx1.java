fec4852da86d1980befcdfd7aa74370fc0943e98
Hyunsoo <jing07161@gmail.com>
2020-05-13 15:37:49 +0900

    Iterator , ListIterator, Enumeration 공부

---
+++ Collections Framework/src/IteratorEx1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorEx1 {
+    public static void main(String[] args) {
+
+    }
+}
+
--- Collections Framework/src/com/company/Main.java
+++ Collections Framework/src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList list = new ArrayList();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+
+        Iterator it = list.iterator();
+        while(it.hasNext()){
+            Object obj = it.next();
+            System.out.println(obj);
+        }
+
+        System.out.println(" == Set 으로 구현 ==" );
+
+        Set s = new HashSet();
+        s.add("1");
+        s.add("2");
+        s.add("3");
+        s.add("4");
+        s.add("5");
+
+
+        Iterator it2 = s.iterator();
+        while(it2.hasNext()){
+            System.out.println(it2.next());
+        }
+
     }
 }
