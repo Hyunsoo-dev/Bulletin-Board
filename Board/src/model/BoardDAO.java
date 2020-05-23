5b20099849bdd682be66343256d6bd878f5d9b8a
Hyun Soo <jing07161@gmail.com>
2020-05-23 17:24:48 +0900

    BoardBean, BoardDAO class 생성

---
+++ Board/src/model/BoardDAO.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BoardDAO {
+
+	
+	Connection con;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	
+	
+	
+	public void getCon() {
+		try {
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String id = "system";
+			String pw = "1234";
+			String driver = "oracle.jdbc.driver.OracleDriver";
+		
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, id, pw);
+				
+		
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} 
+}
